cdac1be923e94684601bb10a5bf81fa8e64ef614
Dagobah-Ubuntu <nicolas.lair@cloud-temple.com>
2020-03-26 16:30:58 +0100

    WIP - Add Things

--- src/simulator/Items.py
+++ src/simulator/Items.py
@@ -2,12 +2,16 @@ class MethodUnavailableError(Exception):
     pass
 
 
+INCREASE_DECREASE_STEP = 10
+
+
 def check_method_availability(func):
     """
     Decorator to check if a method is available for an instance of item
     :param func:
     :return:
     """
+
     def wrapper_check(self, *args, **kwargs):
         if self.methods[func.__name__]:
             func(*args, **kwargs)
@@ -24,7 +28,10 @@ class AbstractItem:
         del self.methods['self']
 
     def get_state(self):
-        pass
+        raise NotImplementedError
+
+    def set_state(self, value):
+        raise NotImplementedError
 
     def setAttribute(self, attribute, value, valuetest, error_message, success_message=""):
         try:
@@ -48,6 +55,13 @@ class ColorItem(AbstractItem):
     def get_state(self):
         return [self.hue, self.saturation, self.brightness]
 
+    def set_state(self, value):
+        h, s, b = value
+        assertion_test = isinstance(h, int) and isinstance(s, int) and isinstance(b, int) and (0 <= h <= 360) and (
+                0 <= s <= 100) and (0 <= b <= 100)
+        error_message = "h, s, b should be positive int, h <=360, s <=100, b<=100"
+        self.setAttribute(['hue', 'saturation', 'brightness'], [h, s, b], assertion_test, error_message)
+
     @check_method_availability
     def turnOn(self):
         self.brightness = self.brightness if self.brightness != 0 else 100
@@ -58,11 +72,11 @@ class ColorItem(AbstractItem):
 
     @check_method_availability
     def increase(self):
-        self.brightness = min(self.brightness + 10, 100)
+        self.brightness = min(self.brightness + INCREASE_DECREASE_STEP, 100)
 
     @check_method_availability
     def decrease(self):
-        self.brightness = max(self.brightness - 10, 0)
+        self.brightness = max(self.brightness - INCREASE_DECREASE_STEP, 0)
 
     @check_method_availability
     def setPercent(self, percent):
@@ -72,23 +86,33 @@ class ColorItem(AbstractItem):
 
     @check_method_availability
     def setHSB(self, h, s, b):
-        assertion_test = isinstance(h, int) and isinstance(s, int) and isinstance(b, int) and (0 <= h <= 360) and (
-                0 <= s <= 100) and (0 <= b <= 100)
-        error_message = "h, s, b should be positive int, h <=360, s <=100, b<=100"
-        self.setAttribute(['hue', 'saturation', 'brightness'], [h, s, b], assertion_test, error_message)
+        self.set_state((h, s, b))
 
 
 class ContactItem(AbstractItem):
-    def __init__(self, OpenClose=False):
+    def __init__(self, OpenClose=False, Open=False, Close=False):
         super().__init__(type="contact", methods=locals())
         self.state = 0
 
     def get_state(self):
         return [self.state]
 
+    def set_state(self, value):
+        assertion_test = (value == 1) or (value == 0)
+        error_message = "onoff should be 0 or 1 (boolean)"
+        self.setAttribute(['onoff'], [value], assertion_test, error_message)
+
     @check_method_availability
     def OpenClose(self):
         self.state = 1 - self.state
+
+    @check_method_availability
+    def Open(self):
+        self.state = 0  # No contact
+
+    @check_method_availability
+    def Close(self):
+        self.state = 1  # Contact
 
 
 class DimmerItem(AbstractItem):
@@ -98,6 +122,11 @@ class DimmerItem(AbstractItem):
 
     def get_state(self):
         return [self.percent]
+
+    def set_state(self, value):
+        assertion_test = isinstance(value, int) and (0 <= value <= 100)
+        error_message = "Percent should be an int between 0 and 100"
+        self.setAttribute(['percent'], [value], assertion_test, error_message)
 
     @check_method_availability
     def turnOn(self):
@@ -109,17 +138,15 @@ class DimmerItem(AbstractItem):
 
     @check_method_availability
     def increase(self):
-        self.percent = min(self.percent + 10, 100)
+        self.percent = min(self.percent + INCREASE_DECREASE_STEP, 100)
 
     @check_method_availability
     def decrease(self):
-        self.percent = max(self.percent - 10, 0)
+        self.percent = max(self.percent - INCREASE_DECREASE_STEP, 0)
 
     @check_method_availability
     def setPercent(self, percent):
-        assertion_test = isinstance(percent, int) and (0 <= percent <= 100)
-        error_message = "Percent should be an int between 0 and 100"
-        self.setAttribute(['percent'], [percent], assertion_test, error_message)
+        self.set_state(percent)
 
 
 class LocationItem(AbstractItem):
@@ -132,12 +159,16 @@ class LocationItem(AbstractItem):
     def get_state(self):
         return [self.latitude, self.longitude, self.altitude]
 
-    @check_method_availability
-    def setLocation(self, lat, lon, alt):
+    def set_state(self, value):
+        lat, lon, alt = value
         assertion_test = isinstance(lat, float) and isinstance(lon, float) and isinstance(alt, float) and (
                 -90 <= lat <= 90) and (-180 <= lon <= 180) and (-500 <= alt <= 4000)
         error_message = "h, s, b should be positive int, h <=360, s <=100, b<=100"
         self.setAttribute(['latitude', 'longitude', 'altitude'], [alt, lon, alt], assertion_test, error_message)
+
+    @check_method_availability
+    def setLocation(self, lat, lon, alt):
+        self.set_state((lat, lon, alt))
 
 
 class NumberItem(AbstractItem):
@@ -148,11 +179,14 @@ class NumberItem(AbstractItem):
     def get_state(self):
         return [self.value]
 
+    def set_state(self, value):
+        assertion_test = isinstance(value, int)
+        error_message = "value should be an int"
+        self.setAttribute(['value'], [value], assertion_test, error_message)
+
     @check_method_availability
     def setValue(self, value):
-        assertion_test = isinstance(value, int)
-        error_message = "Percent should be an int"
-        self.setAttribute(['value'], [value], assertion_test, error_message)
+        self.set_state(value)
 
     # TODO
     @check_method_availability
@@ -160,7 +194,7 @@ class NumberItem(AbstractItem):
         pass
 
 
-class PlayerItem(AbstractItem):
+class PlayerItem(AbstractItem):  # TODO Check state of players
     def __init__(self, PlayPause=False, next=False, previous=False, rewind=False, fastforward=False):
         super().__init__(type="number", methods=locals())
         self.playpause = False
@@ -198,6 +232,11 @@ class RollerShutterItem(AbstractItem):
     def get_state(self):
         return [self.percent]
 
+    def set_state(self, value):
+        assertion_test = isinstance(value, int) and (0 <= value <= 100)
+        error_message = "Percent should be an int between 0 and 100"
+        self.setAttribute(['percent'], [value], assertion_test, error_message)
+
     @check_method_availability
     def up(self):
         pass
@@ -216,9 +255,7 @@ class RollerShutterItem(AbstractItem):
 
     @check_method_availability
     def setPercent(self, percent):
-        assertion_test = isinstance(percent, int) and (0 <= percent <= 100)
-        error_message = "Percent should be an int between 0 and 100"
-        self.setAttribute(['percent'], [percent], assertion_test, error_message)
+        self.set_state(percent)
 
 
 class StringItem(AbstractItem):
@@ -229,22 +266,38 @@ class StringItem(AbstractItem):
     def get_state(self):
         return [self.string]
 
+    def set_state(self, value):
+        assertion_test = isinstance(value, str)
+        error_message = "String should be of type string"
+        self.setAttribute(['string'], [value], assertion_test, error_message)
+
     @check_method_availability
     def setString(self, string):
-        assertion_test = isinstance(string, str)
-        error_message = "String should be of type string"
-        self.setAttribute(['string'], [string], assertion_test, error_message)
+        self.set_state(string)
 
 
 class SwitchItem(AbstractItem):
 
-    def __init__(self, OnOff=False):
+    def __init__(self, OnOff=False, On=False, Off=False):
         super().__init__(type="switch", methods=locals())
         self.onoff = 0
 
     def get_state(self):
         return [self.onoff]
 
+    def set_state(self, value):
+        assertion_test = (value == 1) or (value == 0)
+        error_message = "onoff should be 0 or 1 (boolean)"
+        self.setAttribute(['onoff'], [value], assertion_test, error_message)
+
     @check_method_availability
     def OnOff(self):
         self.onoff = 1 - self.onoff
+
+    @check_method_availability
+    def On(self):
+        self.onoff = 1
+
+    @check_method_availability
+    def Off(self):
+        self.onoff = 0
--- src/simulator/Thing.py
+++ src/simulator/Thing.py
@@ -5,10 +5,89 @@ class Thing:
     def __init__(self):
         pass
 
+    # TODO
+    def get_channels(self):
+        pass
 
-class TV(Thing):
-    def __init__(self):
+
+class Channel:
+    def __init__(self, name, description, item, value=None, read=True, write=True):
+        self.name = name
+        self.description = description
+
+        self.item = item
+        self.item.set_state(value)
+
+        self.read = read
+        self.write = write
+
+
+class LGTV(Thing):
+    """
+    https://www.openhab.org/addons/bindings/lgwebos/
+    """
+
+    def __init__(self, power=1, mute=0):
         super().__init__()
+        self.power = Channel(
+            name='power',
+            description="Current power setting. TV can only be powered off, not on.",
+            item=SwitchItem(Off=True),
+            value=power
+        )
+
+        self.mute = Channel(
+            name="mute",
+            description="Current mute setting.",
+            item=SwitchItem(OnOff=True),
+            value=mute
+        )
+
+        self.volume = Channel(
+            name="volume",
+            description="Current volume setting. Setting and reporting absolute percent values only works when using internal speakers. When connected to an external amp, the volume should be controlled using increase and decrease commands.",
+            item=DimmerItem(increase=True, decrease=True, setPercent=True)
+        )
+
+        self.channel = Channel(
+            name="channel",
+            description="Current channel. Use only the channel number as command to update the channel.",
+            item=StringItem(setString=True)  # TODO maybe discretize this or use Number item
+        )
+
+        self.toast = Channel(
+            name='toast',
+            description="Displays a short message on the TV screen. See also rules section.",
+            item=StringItem(setString=True)  # TODO Copy and paste ?
+        )
+
+        self.mediaPlayer = Channel(
+            name="mediaplayer",
+            description="Media control player",
+            item=PlayerItem(PlayPause=True, next=True, previous=True),  # TODO how to visualize state ?
+            read=False
+        )
+
+        self.mediaStop = Channel(
+            name="mediastop",
+            description="Media control stop",
+            item=SwitchItem(Off=True),
+            read=False
+        )
+
+        self.appLauncher = Channel(
+            name='applauncher',
+            description="Application ID of currently running application. This also allows to start applications on the TV by sending a specific Application ID to this channel.",
+            item=StringItem(setString=True)
+        )
+
+        # Ignore this for now
+        # self.rcButton = Channel(
+        #     name='rcButton',
+        #     description="Simulates pressing of a button on the TV's remote control. See below for a list of button names.",
+        #     item=StringItem(setString=True),
+        #     read=False
+        # )
 
 
 class LightBulb(Thing):
@@ -22,17 +101,50 @@ class LightBulb(Thing):
             name='color',
             description="This channel supports full color control with hue, saturation and brightness values",
             item=ColorItem(turnOn=True, turnOff=True, increase=True, decrease=True, setPercent=True,
-                           setHSB=True))
+                           setHSB=True),
+            value=(0, 0, 0)
+        )
+
         self.color_temperature = Channel(
             name='color_temperature',
             description='This channel supports adjusting the color temperature from cold (0%) to warm (100%)',
             item=DimmerItem(increase=True, decrease=True, setPercent=True)
         )
-        self.alert = Channel(
-            name='alert',
-            description='This channel supports displaying alerts by flashing the bulb either once or multiple times. Valid values are: NONE, SELECT and LSELECT.',
-            item=StringItem()
-        )
+
+        # # TODO decide if use or not : maybe not
+        # self.alert = Channel(
+        #     name='alert',
+        #     description='This channel supports displaying alerts by flashing the bulb either once or multiple times. Valid values are: NONE, SELECT and LSELECT.',
+        #     item=StringItem()
+        # )
+
+
+class PlugSwitch(Thing):
+    """
+    https://www.openhab.org/addons/bindings/zwave/thing.html?manufacturer=everspring&file=an180_0_0.html
+    """
+
+    def __init__(self):
+        super(PlugSwitch, self).__init__()
+        self.switch_binary = Channel(name="switch_binary",
+                                     description="Switch the power on and off.",
+                                     item=SwitchItem(OnOff=True))
+
+        # Ignore both channel
+        # self.alarm = Channel(
+        #     name='alarm_general',
+        #     description="Indicates if an alarm is triggered.",
+        #     item=SwitchItem(),
+        #     write=False,
+        # )
+        #
+        # self.alarm_power = Channel(
+        #     name='alarm_power',
+        #     description="Indicates if a power alarm is triggered.",
+        #     item=SwitchItem(),
+        #     write=False,
+        # )
+
 
 
 class Speaker(Thing):
@@ -48,11 +160,3 @@ class Store(Thing):
 class Chromecast(Thing):
     def __init__(self):
         super(Chromecast, self).__init__()
-
-
-class Channel:
-    def __init__(self, name, description, item, value=None):
-        self.name = name
-        self.description = description
-        self.item = item
-        self.value = value
