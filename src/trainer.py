aba53486ef2de76c72eb4aa69651923a4b9d051f
Nicolas Deathstar <nicolas.lair@cloud-temple.com>
2020-09-08 18:06:36 +0200

    HACK for random RuntimeError bug

--- src/trainer.py
+++ src/trainer.py
@@ -103,79 +103,85 @@ def save_records():
 
 if __name__ == "__main__":
     for i in range(N_SIMULATION):
-        if i != 0:
-            setup_new_simulation(rootLogger, params)
-            
-        save_config(params)
-        logger.info(f'Simulation params:\n {pformat(format_config(params))}')
+        # TODO Fix Hack
+        while True:
+            try:
+                if i != 0:
+                    setup_new_simulation(rootLogger, params)
 
-        test_record = {}
-        env = IoTEnv4ML(params=params['env_params'])
-        test_env = IoTEnv4ML(params=params['env_params'])
+                save_config(params)
+                logger.info(f'Simulation params:\n {pformat(format_config(params))}')
 
-        oracle = Oracle(env=env)
-        language_model = LanguageModel(**params['language_model_params'])
-        agent = DQNAgent(language_model=language_model, params=params)
+                test_record = {}
+                env = IoTEnv4ML(params=params['env_params'])
+                test_env = IoTEnv4ML(params=params['env_params'])
 
-        num_episodes = params['n_episode']
-        deep_action_space_embedding_size = params['model_params']['action_embedding_size']
+                oracle = Oracle(env=env)
+                language_model = LanguageModel(**params['language_model_params'])
+                agent = DQNAgent(language_model=language_model, params=params)
 
-        # (state, available_actions) = env.reset()
-        for j in range(num_episodes):
-            logger.info('%' * 5 + f' Episode {j} ' + '%' * 5)
-            if params['episode_reset']:
-                # Initialize the environment and state + flatten
-                env.reset()
-            else:
-                raise NotImplementedError
-            # TODO go to root action but no global reset
-            target_goal = agent.sample_goal()
-            logger.info(f'Targeted goal: {target_goal.goal_string}')
-            while oracle.is_achieved(state=env.user_state, instruction=target_goal.goal_string):
-                env.reset()
+                num_episodes = params['n_episode']
+                deep_action_space_embedding_size = params['model_params']['action_embedding_size']
 
-            final_state, final_action, final_next_state, ante_final_hidden_state, ante_final_action = run_episode(
-                agent=agent, env=env,
-                target_goal=target_goal,
-                train=True)
+                # (state, available_actions) = env.reset()
+                for j in range(num_episodes):
+                    logger.info('%' * 5 + f' Episode {j} ' + '%' * 5)
+                    if params['episode_reset']:
+                        # Initialize the environment and state + flatten
+                        env.reset()
+                    else:
+                        raise NotImplementedError
+                    # TODO go to root action but no global reset
+                    target_goal = agent.sample_goal()
+                    logger.info(f'Targeted goal: {target_goal.goal_string}')
+                    while oracle.is_achieved(state=env.user_state, instruction=target_goal.goal_string):
+                        env.reset()
 
-            # TODO refactoring to let the goal sampler ask to the oracle
-            achieved_goals_str = oracle.get_state_change(env.previous_user_state, env.user_state)
+                    final_state, final_action, final_next_state, ante_final_hidden_state, ante_final_action = run_episode(
+                        agent=agent, env=env,
+                        target_goal=target_goal,
+                        train=True)
 
-            agent.goal_sampler.update([target_goal], achieved_goals_str, iter=j)
+                    # TODO refactoring to let the goal sampler ask to the oracle
+                    achieved_goals_str = oracle.get_state_change(env.previous_user_state, env.user_state)
 
-            failed_goals = agent.goal_sampler.get_failed_goals(achieved_goals_str=achieved_goals_str)
-            achieved_goals = [agent.goal_sampler.discovered_goals[g] for g in achieved_goals_str]
+                    agent.goal_sampler.update([target_goal], achieved_goals_str, iter=j)
 
-            agent.store_final_transitions(achieved_goals=achieved_goals,
-                                          failed_goals=failed_goals,
-                                          state=final_state,
-                                          action=final_action,
-                                          next_state=(final_next_state, []),
-                                          hidden_state=ante_final_hidden_state,
-                                          previous_action=ante_final_action
-                                          )
+                    failed_goals = agent.goal_sampler.get_failed_goals(achieved_goals_str=achieved_goals_str)
+                    achieved_goals = [agent.goal_sampler.discovered_goals[g] for g in achieved_goals_str]
 
-            agent.update(j)
-            # logger.debug('Update of policy net')
-            # agent.update_policy_net()
-            # logger.debug('done')
-            #
-            # agent.update_exploration_function(n=i + 1)
-            #
-            # if i % params['target_update_frequence'] == 0:
-            #     logger.debug('Update of target net')
-            #     agent.update_target_net()
-            #     logger.debug('done')
+                    agent.store_final_transitions(achieved_goals=achieved_goals,
+                                                  failed_goals=failed_goals,
+                                                  state=final_state,
+                                                  action=final_action,
+                                                  next_state=(final_next_state, []),
+                                                  hidden_state=ante_final_hidden_state,
+                                                  previous_action=ante_final_action
+                                                  )
 
-            if j > 0 and j % params['test_frequence'] == 0:
-                test_record[j] = test_agent(agent=agent, test_env=test_env, oracle=oracle)
+                    agent.update(j)
+                    # logger.debug('Update of policy net')
+                    # agent.update_policy_net()
+                    # logger.debug('done')
+                    #
+                    # agent.update_exploration_function(n=i + 1)
+                    #
+                    # if i % params['target_update_frequence'] == 0:
+                    #     logger.debug('Update of target net')
+                    #     agent.update_target_net()
+                    #     logger.debug('done')
 
-            if j > 0 and j % 10 * params['test_frequence'] == 0:
+                    if j > 0 and j % params['test_frequence'] == 0:
+                        test_record[j] = test_agent(agent=agent, test_env=test_env, oracle=oracle)
+
+                    if j > 0 and j % 10 * params['test_frequence'] == 0:
+                        save_records()
+
+                test_record[num_episodes] = test_agent(agent=agent, test_env=test_env, oracle=oracle)
                 save_records()
 
-        test_record[num_episodes] = test_agent(agent=agent, test_env=test_env, oracle=oracle)
-        save_records()
-
-        end_time = str(datetime.now(tz=None)).split('.')[0]
-        print(f'Completed at {end_time}')
+                end_time = str(datetime.now(tz=None)).split('.')[0]
+                print(f'Completed at {end_time}')
+                break
+            except RuntimeError:
+                pass
